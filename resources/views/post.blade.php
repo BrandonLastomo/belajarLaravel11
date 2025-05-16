050aa78eccbbd540b907fc3c35ba519f84335486
Brandon <brandonlastomo1@gmail.com>
2025-05-17 04:44:44 +0700

    fix fill datas in database and fix date display

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,7 +4,10 @@
     <article class="py-8 max-w-screen-md">
       <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
       <div class="text-base text-gray-500">
-        <a href="#">{{ $post['author'] }}</a> | 11 Mei 2025
+        {{-- pakai dateformat --}}
+        <a href="#">{{ $post['author'] }}</a> | {{ $post['created_at']->format('D, d F Y') }}
+        {{-- pakai diffForHumans --}}
+        <a href="#">{{ $post['author'] }}</a> | {{ $post['created_at']->diffForHumas() }}
       </div>
       <p class="my-4 font-light">
         {{ $post['body']}}
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,7 +7,10 @@
         <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
       </a>
       <div class="text-base text-gray-500">
-        <a href="#">{{ $post['author'] }}</a> | 11 Mei 2025
+        {{-- pakai dateformat --}}
+        <a href="#">{{ $post['author'] }}</a> | {{ $post['created_at']->format('D, d F Y') }}
+        {{-- pakai diffForHumans --}}
+        <a href="#">{{ $post['author'] }}</a> | {{ $post['created_at']->diffForHumas() }}
       </div>
       <p class="my-4 font-light">
         {{ Str::limit($post['body'], 150) }}
